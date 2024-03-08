3cc499e48fe942264f56e64c69cda81da3d34a4f
j0hannes <j0hannes@sfc.wide.ad.jp>
2024-03-08 18:25:38 +0900

    add another error handling

--- PhChkr4Chrome.py
+++ PhChkr4Chrome.py
@@ -12,17 +12,19 @@ skip_n = 0
 csv_file_path = r'C:\user2\Desktop\autosearchurl\url.csv'
 user_data_dir = r'C:\Users\[ユーザー名]\AppData\Local\Google\Chrome\User Data'
 
-# Chromeのオプション設定
-options = Options()
-options.add_argument(f"user-data-dir={user_data_dir}")
-options.add_argument('--profile-directory=Default')
+def init_driver():
+    # Chromeのオプション設定
+    options = Options()
+    options.add_argument(f"user-data-dir={user_data_dir}")
+    options.add_argument('--profile-directory=Default')
 
-# 必要に応じてオプションを追加
-# options.add_argument('--headless')  # ヘッドレスモードを有効にする場合
+    # 必要に応じてオプションを追加
+    # options.add_argument('--headless')  # ヘッドレスモードを有効にする場合
+
+    return webdriver.Chrome(options=options)
 
 # WebDriverの初期化
-driver = webdriver.Chrome(options=options)
-
+driver = init_driver()
 #timeout setting
 driver.set_page_load_timeout(5)
 driver.set_script_timeout(5)
@@ -43,26 +45,29 @@ def main():
 
         for row in reader:
             url = row[1]  # URLを読み込む
+            exe_count += 1
+            print(f"{exe_count}, {url}")
             try:
-                exe_count += 1
-                print(f"{exe_count}, {url}")
                 # URLを新しいタブで開く
                 driver.execute_script("window.open('');")  # 新しいタブを開く
                 driver.switch_to.window(driver.window_handles[-1])  # 新しいタブに切り替え
                 driver.get(url)  # URLを開く
 
                 # URLを開いた後の処理（必要に応じて）
-                
+            except Exception as e:
+                print(f"URL: {url}, エラーが発生しました: {e.msg}")
+                pass
+
+            try:
                 # タブを閉じて、最初のタブに戻る
                 driver.close()  # 現在のタブを閉じる
                 driver.switch_to.window(driver.window_handles[0])  # 最初のタブに戻る
             except Exception as e:
-                print(f"URL: {url}, エラーが発生しました: {e}")
+                print(f"URL: {url}, タブを閉じる際にエラーが発生しました: {e.msg}")
                 #Reopen the driver in case of error occuring.
                 driver.quit()
-                driver = webdriver.Chrome(options=options)
+                driver = init_driver()
                 continue  # 次のループへ進む
-            
 
     # 全てのURLの処理が終わったら、ブラウザを閉じる
     driver.quit()
