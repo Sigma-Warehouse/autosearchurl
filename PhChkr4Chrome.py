080940fa98fab8e97fd52bfe53bad5b34de39720
j0hannes <j0hannes@sfc.wide.ad.jp>
2024-03-10 18:22:59 +0900

    output result to csv file

--- PhChkr4Chrome.py
+++ PhChkr4Chrome.py
@@ -1,12 +1,17 @@
-import time
 import csv
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
 
 #skip rows
 skip_n = 0
+
+# Define CSV file path
+csv_result_path = "output.csv"
+
+# Define CSV column headers
+fieldnames = ["id", "url", "status", "chrome", "layerx", "error", "redirections"]
 
 # CSVファイルのパス
 csv_file_path = r'C:\user2\Desktop\autosearchurl\url.csv'
@@ -18,19 +23,48 @@ def init_driver():
     options.add_argument(f"user-data-dir={user_data_dir}")
     options.add_argument('--profile-directory=Default')
 
-    # 必要に応じてオプションを追加
-    # options.add_argument('--headless')  # ヘッドレスモードを有効にする場合
+    # ヘッドレスモードを有効にする場合
+    # options.add_argument('--headless')
 
     return webdriver.Chrome(options=options)
 
-# WebDriverの初期化
-driver = init_driver()
-#timeout setting
-driver.set_page_load_timeout(5)
-driver.set_script_timeout(5)
+def check_safe_search(n):
+    global driver
+    
+    flag = False
+    try:
+        # Click the "details-button"
+        details_button = driver.find_element(By.ID, "details-button")
+        details_button.click()
+
+        # Click the "proceed-link"
+        proceed_link = driver.find_element(By.ID, "proceed-link")
+        proceed_link.click()
+
+        flag = True
+
+        #limit the number of redirects to 100 or less.
+        if n > 100:
+            return flag, n
+
+        return check_safe_search(n+1)
+    except:
+        return flag, n
+
+def check_layerx():
+    global driver
+
+    try:
+        driver.find_element(By.XPATH, '//lit-block-alert[contains(@content, "ALsw3b12!")]')
+        return True
+    except:
+        return False
 
 def main():
     global driver
+
+    #For result
+    data = []
 
     #Execution count
     exe_count = 0
@@ -46,6 +80,10 @@ def main():
         for row in reader:
             url = row[1]  # URLを読み込む
             exe_count += 1
+            error_flg = False
+            safe_search = False
+            layerx = False
+            redirections = 0
             print(f"{exe_count}, {url}")
             try:
                 # URLを新しいタブで開く
@@ -54,8 +92,11 @@ def main():
                 driver.get(url)  # URLを開く
 
                 # URLを開いた後の処理（必要に応じて）
+                safe_search, redirections = check_safe_search(0)
+                layerx = check_layerx()
             except Exception as e:
                 print(f"URL: {url}, エラーが発生しました: {e.msg}")
+                error_flg = True
                 pass
 
             try:
@@ -67,10 +108,30 @@ def main():
                 #Reopen the driver in case of error occuring.
                 driver.quit()
                 driver = init_driver()
-                continue  # 次のループへ進む
+            finally:
+                result = {"id": row[0], "url": row[1], "status": row[2], "chrome": safe_search, "layerx": layerx, "error": error_flg, "redirections": redirections}
+                data.append(result)
 
     # 全てのURLの処理が終わったら、ブラウザを閉じる
     driver.quit()
 
+    # Write data to CSV file
+    with open(csv_result_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write header
+        writer.writeheader()
+
+        # Write data rows
+        for row in data:
+            writer.writerow(row)
+
 if __name__ == '__main__':
+    # WebDriverの初期化
+    driver = init_driver()
+    #timeout setting
+    driver.set_page_load_timeout(5)
+    driver.set_script_timeout(5)
+    driver.implicitly_wait(2)
+
     main()
